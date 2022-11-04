eeac8b53f61c6d4e4844f616d3881d53cb45811f
Denis <denis.rovdo@yandex.com>
2022-11-04 11:41:55 +0100

    feat: completed second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const {
+    stdin,
+    stdout
+} = process;
+
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
+
+stdout.write('write some text\n');
+
+const endInput = () => {
+    stdout.write('good bye');
+    process.exit();
+};
+
+const writeText = (data) => {
+    const exitText = data.toString().trim();
+
+    if (exitText === 'exit') {
+        endInput();
+    }
+
+    writeStream.write(data);
+};
+
+
+stdin.on('data', writeText);
+process.on("SIGINT", endInput);
