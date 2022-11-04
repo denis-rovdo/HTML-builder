01c33c9637e6dd1c76c00f19bda31ee2917912c1
Denis <denis.rovdo@yandex.com>
2022-11-04 17:13:04 +0100

    feat: completed the third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+
+const folderPath = path.resolve(__dirname, 'secret-folder');
+
+try {
+
+    (async () => {
+        const files = await fs.readdir(folderPath, {withFileTypes: true});
+    
+
+        for(let i = 0; i<files.length; i++){
+            
+            let item = files[i];
+
+            if(item.isFile()){
+                const stats =  ((await fs.stat(path.resolve(folderPath, item.name))).size)/1024;
+                const nameFile = item.name.replace(/\..*/, '');
+                const type = path.extname(item.name).replace('.','');
+                console.log(`${nameFile} - ${type} - ${stats.toFixed(2)}kb`);
+            }
+        }
+       
+    })();
+
+} catch (err) {
+    console.log(err);
+}
